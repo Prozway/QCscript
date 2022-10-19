5c32a039f98d87e31d8cc3bde9d1ade82af9631e
Prozway <111115961+Prozway@users.noreply.github.com>
2022-10-19 17:22:10 +0200

    Create QCSCRIPT_MAIN.py

---
+++ QCSCRIPT_MAIN.py
@@ -0,0 +1,7 @@
+def QCS_MAIN_RUN():
+  import time
+  import random
+  QCS_MAIN_RUN_VAR = "-m (-qcs)"+"FL_LOCK"+38628743873838482
+  print(QCS_MAIN_RUN_VAR)
+color_QCS_Id = "Undefined"+" "+QCS_MAIN_RUN_VAR"+"UD"
+QCS_MAIN_RUN()
